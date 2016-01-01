7e08c5422128fd4c36e558c3c1e5fb456a0ff410
Bruno Souza <brunoprogramadorjava>
2016-01-01 14:54:54 -0200

    Deve continuar a execucao mesmo quando o enviador de email falhar

--- test/br/com/caelum/leilao/servico/EncerradorDeLeilaoTest.java
+++ test/br/com/caelum/leilao/servico/EncerradorDeLeilaoTest.java
@@ -162,5 +162,30 @@ public class EncerradorDeLeilaoTest {
     	
     }
     
+    @Test
+    public void deveContinuarAExecucaoMesmoQuandoEnviadorDeEmaillFalha() {
+        Calendar antiga = Calendar.getInstance();
+        antiga.set(1999, 1, 20);
+
+        Leilao leilao1 = new CriadorDeLeilao().para("TV de plasma")
+            .naData(antiga).constroi();
+        Leilao leilao2 = new CriadorDeLeilao().para("Geladeira")
+            .naData(antiga).constroi();
+
+        RepositorioDeLeiloes daoFalso = mock(RepositorioDeLeiloes.class);
+        when(daoFalso.correntes()).thenReturn(Arrays.asList(leilao1, leilao2));
+
+        EnviadorDeEmail carteiroFalso = mock(EnviadorDeEmail.class);
+        doThrow(new RuntimeException()).when(carteiroFalso).envia(leilao1);
+
+        EncerradorDeLeilao encerrador =
+            new EncerradorDeLeilao(daoFalso, carteiroFalso);
+
+        encerrador.encerra();
+
+        verify(daoFalso).atualiza(leilao2);
+        verify(carteiroFalso).envia(leilao2);
+    }
+    
 
 }
